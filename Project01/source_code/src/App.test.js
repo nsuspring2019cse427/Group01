6eb45e48298e2523317e1c5d7e1fd6b740fdc8b5
TusharMazhar <tushar.talukdar@northsouth.edu>
2019-03-19 04:46:26 -0700

    tested App.js properly rendered using jest expect method

--- Project01/source_code/src/App.test.js
+++ Project01/source_code/src/App.test.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {
+    shallow
+} from 'enzyme';
 import App from './App';
 
-it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render( < App / > , div);
+it('App renders without crashing', () => {
+    const component = shallow( < App / > );
+    expect(component.exists()).toEqual(true);
 });
