3ac6e1ed0a14ae31dc382bbca3dbd1b52fe7c8e7
TusharMazhar <tushar.talukdar@northsouth.edu>
2019-03-19 05:18:43 -0700

    tested file using enzyme

--- Project01/source_code/src/App.test.js
+++ Project01/source_code/src/App.test.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import {
+    configure,
     shallow
 } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({
+    adapter: new Adapater()
+});
 import App from './App';
 
 it('App renders without crashing', () => {
     const component = shallow( < App / > );
     expect(component.exists()).toEqual(true);
+
 });
